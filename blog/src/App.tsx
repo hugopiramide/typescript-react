c70b363029591a1615f6c9dccc41ece080256e48
hugopiramide <hugocastillo.deus@gmail.com>
2025-11-11 09:12:10 +0100

    Add paths routes

--- blog/src/App.tsx
+++ blog/src/App.tsx
@@ -1,9 +1,32 @@
-import { useState } from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
+
+const router = createBrowserRouter([
+{
+  path:'/home',
+  element: <Home />,
+  errorElement: <div>404 not found</div>
+},
+{
+  path:'/users',
+  element: <Users />
+},
+{
+  path:'/posts',
+  element: <Posts />
+},
+{
+  path:'/register',
+  element: <Register />
+}
+])
 
 function App() {
   return (
     <>
+      <Header />
+        
+      <Footer />
     </>
   )
 }
