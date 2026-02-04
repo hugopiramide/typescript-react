79363617acf2687938c7119b28b29c7054ba5598
hugopiramide <hugocastillo.deus@gmail.com>
2026-02-04 17:13:00 +0100

    Update DTOs

--- shop/src/types/types.ts
+++ shop/src/types/types.ts
@@ -1,10 +1,25 @@
-export interface Product {
+export interface ProductResponse {
   id: number;
   name: string;
-  category: string;
-  price: number;
-  imageURL: string;
+  category: CategoryResponse;
+  basePrice: number;
+  imageUrl: string;
   description: string;
+  active: boolean;
+  variants: ProductVariantResponse[];
+}
+
+export interface CategoryResponse {
+  id: number;
+  name: string;
+  description: string;
+}
+
+export interface ProductVariantResponse {
+  id: number;
+  size: string;
+  stock: number;
+  priceModifier: number;
 }
 
 export type AuthMode = 'login' | 'register'
